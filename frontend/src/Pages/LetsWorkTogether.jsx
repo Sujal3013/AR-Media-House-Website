88dfe745ab63cdef41053be03bf734fdddfa1f20
Dev-Sahani <devdaksh8102003@gmail.com>
2024-01-03 13:43:59 +0530

    Routes Created

---
+++ frontend/src/Pages/LetsWorkTogether.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function LetsWorkTogether() {
+  return (
+    <div>LetsWorkTogether</div>
+  )
+}
---
+++ frontend/src/Pages/index.jsx
@@ -0,0 +1,19 @@
+import Home from "./Home";
+import Services from "./Services";
+import Technologies from "./Technologies";
+import About from "./About";
+import LetsWorkTogether from "./LetsWorkTogether";
+import Page404 from "./Page404";
+import CaseStudies from "./CaseStudies";
+import SharedLayout from "./SharedLayout";
+
+export {
+    About,
+    CaseStudies,
+    Home, 
+    LetsWorkTogether,
+    Page404,
+    Services,
+    SharedLayout, 
+    Technologies,
+}
