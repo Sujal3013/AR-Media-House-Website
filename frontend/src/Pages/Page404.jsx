e12b9c7864e1b9f34ef9b90d48fee966c67eecdb
Sujal <mishrasujal64@gmail.com>
2024-01-05 11:09:32 +0530

    Page404 modified

--- frontend/src/Pages/Page404.jsx
+++ frontend/src/Pages/Page404.jsx
@@ -7,9 +7,9 @@ export default function Page404() {
 <Navbar/>
     <div className="flex items-center justify-center h-screen bg-gray-100">
     <div className="text-center">
-      <h1 className="text-6xl font-bold text-gray-800">404</h1>
-      <p className="text-2xl text-gray-600">Page Not Found</p>
-      <p className="mt-4 text-gray-500">The page you are looking for might be in another universe.</p>
+      <h1 className="text-6xl font-bold text-secondary-500">404</h1>
+      <p className="text-2xl text-secondary-500">Page Not Found</p>
+      <p className="mt-4 text-secondary-500">The page you are looking for might be in another universe.</p>
       <Button to="/" arrow="true" className="bg-primary-500 text-secondary-500 text-sm mx-auto py-2  px-3 mt-3">
         Back to Home
       </Button>
