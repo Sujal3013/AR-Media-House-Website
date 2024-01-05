dc96e0df8f0eb5843202854f4e994d56a3481d07
Dev-Sahani <devdaksh8102003@gmail.com>
2024-01-05 11:51:33 +0530

    refractoring import and export from pages

---
+++ frontend/src/Pages/index.jsx
@@ -0,0 +1,21 @@
+import Home from "./Home";
+import Technologies from "./Technologies";
+import SharedLayout from "./SharedLayout"
+import ContactUs from "./ContactUs";
+import Page404 from "./Page404";
+import Services from "./Services"
+import About from "./About";
+import Careers from "./Careers";
+import CaseStudies from "./CaseStudies";
+
+export {
+    Home,
+    Technologies,
+    SharedLayout,
+    ContactUs,
+    Page404,
+    Services,
+    About ,
+    Careers,
+    CaseStudies,
+}
