df6a5bb4268b6fc23e7f88030e27388e4e5e5538
Sujal <mishrasujal64@gmail.com>
2024-01-06 12:51:26 +0530

    Wonders.jsx deleted & Careers structure completed

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Wonder from "../components/Wonder";
 import Form from "../components/Form";
 import Button from "../components/Button";
 
@@ -31,32 +30,33 @@ const ContactUs = () => {
         </div>
       </div>
     </div>
-    <div className="bg-secondary-500">
+    <div className="bg-secondary-500 py-6">
       <span className="px-screen-padding flex items-center">
-      
-        <span className="basis-[40rem] py-10">
-          <span className="text-white font-jost font-bold text-7xl">CONTACT</span><br />
-          <span className="text-primary-500 font-playfair-display text-6xl px-2 text-center">Information</span>
+        <span className="basis-[40rem] flex-col py-10 gap-0">
+          <span className="text-white block font-jost font-extrabold text-[60px] tracking-wide leading-8">CONTACT</span>
+          <span className="text-primary-500 font-playfair-display text-[48px] px-2 text-center">Information</span>
         </span>
-        <span className=" flex mt-4 gap-10">
-        <div className="bg-white py-5 px-10 relative top-6 shadow-md rounded-md">
-            <span className="mx-auto">
-              <span className="flex-col">
-                <img src="/assets/location-logo.svg" className="relative top-[-48px]" alt="" />
-                <h1>Address Line 1</h1>
+        <span className=" flex px-5 mt-4 gap-12">
+        <div className="bg-white py-5 px-10 relative top-12 shadow-md rounded-md">
+            <span className="">
+              <span className="flex-col text-center">
+                <img src="/assets/location-logo.svg" className="relative top-[-48px] mx-auto" alt="" />
+                <h1 className="">Address Line 1 </h1>
+                <a href="mailto:">armediahouse@gmail.com</a>
               </span>
-              <Button to="" className="bg-primary-500 relative top-8 p-4 shadow-md rounded-sm">
+              <Button to="" className="bg-primary-500 relative top-8 mx-auto p-4 shadow-md rounded-sm">
                 Get Directions
               </Button>
             </span>
           </div>
-          <div className="bg-white py-5 px-10 relative top-6 shadow-md rounded-md">
+          <div className="bg-white py-5 px-10 relative top-10 shadow-md rounded-md">
             <span className="mx-auto">
-              <span className="flex-col">
-                <img src="/assets/location-logo.svg" className="relative top-[-48px]" alt="" />
+              <span className="flex-col text-center">
+                <img src="/assets/location-logo.svg" className="relative top-[-48px] mx-auto" alt="" />
                 <h1>Address Line 1</h1>
+                <a href="mailto:">armediahouse@gmail.com</a>
               </span>
-              <Button to="" className="bg-primary-500 relative top-8 p-4 shadow-md rounded-sm">
+              <Button to="" className="bg-primary-500 relative top-8 mx-auto p-4 shadow-md rounded-sm">
                 Get Directions
               </Button>
             </span>
@@ -64,10 +64,6 @@ const ContactUs = () => {
         </span>
       </span>
       
-    </div>
-    <div className="bg-white">
-    WONDER'S SECTION
-    <Wonder/>
     </div>
     </>
   );
