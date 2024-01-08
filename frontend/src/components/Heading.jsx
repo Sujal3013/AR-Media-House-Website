fa738098c35ed7d1f25f885df395891a7170e8f6
Dev-Sahani <devdaksh8102003@gmail.com>
2024-01-09 00:10:37 +0530

    Carousel at explore page added, colors modiefied, Home.jsx refractored

--- frontend/src/components/Heading.jsx
+++ frontend/src/components/Heading.jsx
@@ -14,14 +14,14 @@ export default function Heading({
   const classes = classNames("flex flex-col gap-[-2px]", {
     [className]: className,
   })
-  const headingClasses = classNames("font-black text-5xl uppercase", {
+  const headingClasses = classNames("font-black text-6xl uppercase", {
     "text-primary-500": !headingClassName,
     [headingClassName]: headingClassName,
   })
-  const middleLineClasses = classNames("font-bold text-9xl uppercase", {
+  const middleLineClasses = classNames("font-bold text-8xl uppercase", {
     [middleLineClassName]: middleLineClassName,
   });
-  const subHeadingClasses = classNames("font-bold text-4xl mt-[-2px]", {
+  const subHeadingClasses = classNames("font-bold text-5xl mt-[-2px]", {
     "text-secondary-500": !subHeadingClassName,
     [subHeadingClassName]: subHeadingClassName,
   })
