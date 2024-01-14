bc54c8dc503e1ff4e31661eb43b636bffcbf208a
DakshSahani <dakshsahani8123@gmail.com>
2024-01-14 19:20:13 +0530

    Careers Page Responsiveness

--- frontend/src/components/Jobopeningcard.jsx
+++ frontend/src/components/Jobopeningcard.jsx
@@ -3,13 +3,13 @@ import Button from "../components/Button"
 export const Jobopeningcard = ({id=0,title, location, daysAgo }) => {
   return (
     <>
-      <div className={"job-card flex rounded-lg shadow-md shadow-secondary-200 justify-between items-center p-10 odd:bg-white even:bg-tertiary-500 transition-transform transform hover:scale-[1.03] hover:cursor-pointer"}>
-        <div className="text-xl text-secondary-500 mb-2">
+      <div className={"job-card flex flex-col md:flex-row justify-center md:justify-between items-center rounded-lg shadow-md shadow-secondary-200 p-10 odd:bg-white even:bg-tertiary-500 transition-transform transform hover:scale-[1.03] hover:cursor-pointer"}>
+        <div className="text-center md:text-left text-xl text-secondary-500 mb-2">
           <p className="font-semibold mb-4">{title}</p>
           {location}
         </div>
         <div>
-        <div className="text-secondary-300 text-xl mb-2 pt-1">
+        <div className="text-center md:text-left text-secondary-300 text-xl mb-2 pt-1">
           <p className="ml-px">Posted</p> 
           {daysAgo} days ago{" "}
         </div>
