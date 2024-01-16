0d3be38f8acc83930b9351303d9f4fd3c77d35cb
Sujal <mishrasujal64@gmail.com>
2024-01-16 15:33:56 +0530

    About section modified

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,42 +1,43 @@
 import React, { useEffect, useState } from "react";
-import { 
-  Button, 
-  Heading, 
-  StoryComponent, 
-  Section, 
-  Wonder, 
+import classNames from "classnames";
+import {
+  Button,
+  Heading,
+  StoryComponent,
+  Section,
+  Wonder,
   BlackSection,
   HeroSectionWrapper,
   Circle,
-  Triangle
+  Triangle,
 } from "../components";
-import Youtube from 'react-youtube'
+import Youtube from "react-youtube";
 import { useWinSizeContext } from "../context/winSizeContext";
 
-const VIDEO_ID = "_qh5PXngdNY"
+const VIDEO_ID = "_qh5PXngdNY";
 const OPTS = {
-  height: '350',
-  width: '575',
+  height: "350",
+  width: "575",
   playerVars: {
     autoplay: 0,
   },
-}
+};
 
-const ILLUSTRATION_CLASS = "max-w-[465px] rounded-full text-center basis-1/2 h-auto"
+const ILLUSTRATION_CLASS =
+  "max-w-[465px] rounded-full text-center basis-1/2 h-auto";
 
 export default function About() {
   document.querySelector("title").innerHTML = "About - AR Media House";
-  const {winSize , BR_1, BR_2, BR_3} = useWinSizeContext()
+  const { winSize, BR_1, BR_2, BR_3 } = useWinSizeContext();
 
-  if(winSize <= BR_1) {
-    OPTS.height *= 0.7
-    OPTS.width *= 0.7
+  if (winSize <= BR_1) {
+    OPTS.height *= 0.7;
+    OPTS.width *= 0.7;
+  } else if (winSize < BR_2) {
+    OPTS.height *= 0.9;
+    OPTS.width *= 0.9;
   }
-  else if(winSize < BR_2){
-    OPTS.height *= 0.9
-    OPTS.width *= 0.9
-  }
-  
+
   return (
     <>
       <HeroSectionWrapper className="!mb-0" whiteWaveBackground>
@@ -76,7 +77,7 @@ export default function About() {
         textClassName="py-10 "
         illustration={
           <div className="rounded-3xl">
-            <Youtube  videoId={VIDEO_ID} opts={OPTS} />
+            <Youtube videoId={VIDEO_ID} opts={OPTS} />
           </div>
         }
         largeFlex
@@ -89,17 +90,20 @@ export default function About() {
         }
       />
 
-      <img 
+      <img
         src="./assets/triple-wave-white-1.svg"
         alt="wave"
         className="w-full"
       />
 
-
       <section className="bg-tertiary-500 relative">
-        <img src="./assets/about-wave.png" alt="path" className="hidden lg:block absolute top-[1.5%] left-[19%] h-[85%] w-[78%]" /> 
-        
-        <StoryComponent 
+        <img
+          src="./assets/about-wave.png"
+          alt="path"
+          className="hidden lg:block absolute top-[1.5%] left-[19%] h-[85%] w-[78%]"
+        />
+
+        <StoryComponent
           heading="The journey begins"
           text="In 2020, Harish Parihar embarked on a solo journey, offering digital
           marketing services to local clients as a freelancer. This phase served
@@ -108,73 +112,101 @@ export default function About() {
           clientele steadily grew, laying the groundwork for what was to come."
           Image={
             <div className={ILLUSTRATION_CLASS}>
-              <img src="/assets/Story_pic.png" alt=""  className="w-full"/>
+              <img src="/assets/Story_pic.png" alt="" className="w-full" />
             </div>
           }
           direction={1}
           Year={2020}
-          aboutPath={<img src="./assets/about-wave-1.svg" alt="path" className="absolute top-12 left-[45%]" />}
+          aboutPath={
+            <img
+              src="./assets/about-wave-1.svg"
+              alt="path"
+              className="absolute top-12 left-[45%]"
+            />
+          }
         />
-        
-        <StoryComponent 
+
+        <StoryComponent
           heading="Expansion and Franchise Endeavours"
           text="The year 2021 marked a pivotal moment as Harish's client base expanded significantly. Towards the year's end, he took a bold step, delving into a franchise model, forming a partnership with Amazon's Easy Store. This ambitious move involved selling franchises through the renowned platform, signalling a significant leap forward in his entrepreneurial journey."
           Image={
             <div className={ILLUSTRATION_CLASS}>
-              <img src="/assets/Story_pic.png" alt=""  className=""/>
+              <img src="/assets/Story_pic.png" alt="" className="" />
             </div>
           }
           direction={-1}
           Year={2021}
-          aboutPath={<img src="./assets/about-wave-2.svg" alt="path" className="absolute bottom-[-50%] left-[25%]" />}
+          aboutPath={
+            <img
+              src="./assets/about-wave-2.svg"
+              alt="path"
+              className="absolute bottom-[-50%] left-[25%]"
+            />
+          }
         />
 
-        <StoryComponent 
+        <StoryComponent
           heading="Adversity and Pivot"
           text="Unfortunately, 2022 presented unexpected challenges when Amazon discontinued its operations, leading to the closure of the franchise model. Undeterred, Harish pivoted his approach, founding a full-stack digital marketing company, recognizing the need for a broader spectrum of services. The focus shifted towards data generation and expansion, signifying resilience in the face of adversity."
           Image={
             <div className={ILLUSTRATION_CLASS}>
-              <img src="/assets/Story_pic.png" alt=""  className=""/>
+              <img src="/assets/Story_pic.png" alt="" className="" />
             </div>
           }
           direction={1}
           Year={2022}
-          aboutPath={<img src="./assets/about-wave-3.svg" alt="path" className="absolute bottom-[-46%] left-[37%]" />}
+          aboutPath={
+            <img
+              src="./assets/about-wave-3.svg"
+              alt="path"
+              className="absolute bottom-[-46%] left-[37%]"
+            />
+          }
         />
 
-        <StoryComponent 
+        <StoryComponent
           heading="Collaborative Innovations"
           text="In 2023, fate brought Harish together with like-minded visionaries, Raj Maheshwari and Shelendra Mukati, during a summit. Engaging in spirited discussions, they explored innovative ideas, particularly around lead generation. Their synergy and shared aspirations led to the inception of Lead Bank—a revolutionary model empowered by an in-house software boasting unlimited data sources."
           Image={
             <div className={ILLUSTRATION_CLASS}>
-              <img src="/assets/Story_pic.png" alt=""  className=""/>
+              <img src="/assets/Story_pic.png" alt="" className="" />
             </div>
           }
           direction={-1}
           Year={2023}
-          aboutPath={<img src="./assets/about-wave-2.svg" alt="path" className="absolute bottom-[-50%] left-[25%]" />}
+          aboutPath={
+            <img
+              src="./assets/about-wave-2.svg"
+              alt="path"
+              className="absolute bottom-[-50%] left-[25%]"
+            />
+          }
         />
 
-        <StoryComponent 
+        <StoryComponent
           heading="Global Ventures and Sectoral Diversification"
           text="The year 2024 heralded a transformative phase for Lead Bank. With a well-established model in place, they successfully onboarded foreign and domestic clients, amplifying their impact across diverse sectors—from Indian businesses to digital products, ecommerce ventures, and educational initiatives. Their collaborative efforts aimed at empowering entities to thrive in the ever-evolving landscape of the online realm."
           Image={
             <div className={ILLUSTRATION_CLASS}>
-              <img src="/assets/Story_pic.png" alt=""  className=""/>
+              <img src="/assets/Story_pic.png" alt="" className="" />
             </div>
           }
           direction={1}
           Year={2024}
         />
 
-        <img src="./assets/triple-wave-white-1.svg" alt="" className="w-full bg-white rotate-180" />
+        <img
+          src="./assets/triple-wave-white-1.svg"
+          alt=""
+          className="w-full bg-white rotate-180"
+        />
 
         <Circle className="top-[18%] left-[5%]" />
         <Triangle className="top-[52%] right-[5%]" />
         <Triangle className="bottom-[2%] right-[5%]" />
         <Circle className="top-[58%] left-[5%]" />
       </section>
-        
+
       <Section
         backgroundClassName="bg-white py-10"
         heading="We are a team of"
@@ -184,18 +216,129 @@ export default function About() {
         textClassName="my-10"
         illustration={
           // <div className="rounded-full text-center w-[40vw] h-auto">
-            <img src="/assets/Individuals_About.svg" alt="" className="ImgResponsive h-auto w-[35vw]"/>
-            // <img src="/assets/Individuals_About.svg" alt=""  className=""/>
+          <img
+            src="/assets/Individuals_About.svg"
+            alt=""
+            className="ImgResponsive h-auto w-[35vw]"
+          />
+          // <img src="/assets/Individuals_About.svg" alt=""  className=""/>
           // </div>
         }
         Button1={
-          <Button className="bg-primary-500 text-secondary-500 mt-6 mb-12" arrow>
+          <Button
+            className="bg-primary-500 text-secondary-500 mt-6 mb-12"
+            arrow
+          >
             Let's Connect
           </Button>
         }
       />
+      <section className="bg-white py-4 px-screen-padding relative">
+        <div className="flex justify-center md:justify-between flex-wrap md:flex-nowrap gap-y-12 py-6">
+          {cardList.map((el, ind) => (
+            <Card {...el} key={ind} titleClassNames="max-w-64 pt-2 md:pt-16" />
+          ))}
+        </div>
+        <Circle className="top-[-5%] right-[5%]" />
+      </section>
+      <Section
+        heading="WE BRING MORE"
+        subHeading="traffic and conversion to your website"
+        subHeadingClassName="text-white font-playfair-display"
+        illustration={
+          <Button arrow className="bg-primary-500 mx-auto">
+            Learn More
+          </Button>
+        }
+        largeFlex
+        backgroundClassName="bg-black rounded-br-[100px] !py-16"
+        Shapes={
+          <>
+            <Circle className="hidden md:block top-[-24%] left-[50%]" />
+            <Triangle className="bottom-[-10%] left-[20%]" />
+          </>
+        }
+      />
+      <Section
+        heading="STRONG"
+        subHeading="portfolio"
+        subHeadingClassName="text-white font-playfair-display"
+        text="Incorporate details about your brand’s approach and
+        build a robust marketing portfolio."
+        textClassName="text-white pt-6 max-w-96"
+        illustration={
+          <div className="flex justify-between gap-8 items-center">
+            <div className="border-l-4 border-gradient max-w-[20rem]">
+              <p className="text-white px-2">
+                We excel in what we do in that we know what it takes for a brand
+                to grow, we focus on delivering quality results rather than
+                quick results and this is what makes us stand out from our rival
+                companies.
+              </p>
+            </div>
+            <div className="border-l-4 border-gradient max-w-[20rem]">
+              <p className="text-white px-2">
+                We develop a robust relationship with our clients, keep up
+                commitments, and work together to build a strong outcome.
+              </p>
+            </div>
+          </div>
+        }
+        largeFlex
+        backgroundClassName="bg-black rounded-br-[100px] !py-16"
+        Shapes={
+          <>
+            <Circle className="hidden md:block top-[-24%] left-[50%]" />
+          </>
+        }
+      />
 
-      <Wonder className="!bg-tertiary-500" linearGradient={{"backgroundImage": "linear-gradient(0deg, #080403 50%, white 50%)"}}/>
+      <Wonder
+        className="!bg-tertiary-500"
+        linearGradient={{
+          backgroundImage: "linear-gradient(0deg, #080403 50%, white 50%)",
+        }}
+      />
     </>
+  );
+}
+
+const cardList = [
+  {
+    title: "Focus on client's success",
+    blobSrc: "./assets/About-blob-big.svg",
+    iconSrc: "./assets/about-target.svg",
+  },
+  {
+    title: "Elevate one another",
+    blobSrc: "./assets/About-blob-big.svg",
+    iconSrc: "./assets/about-arrow.svg",
+  },
+  {
+    title: "Possess the will to win.",
+    blobSrc: "./assets/About-blob-big.svg",
+    iconSrc: "./assets/about-trophy.svg",
+  },
+];
+
+function Card({ blobSrc, iconSrc, title, titleClassNames }) {
+  const wraperClass = classNames(
+    "flex flex-col items-center",
+    "text-center",
+    "min-h-60",
+    "max-w-64 lg:max-w-80",
+    `bg-no-repeat bg-[0%] bg-cover`
+  );
+
+  const titleClass = classNames(
+    "!text-5xl sm:text-4xl text-secondary-500 font-playfair-display font-black mb-3",
+    titleClassNames
+  );
+
+  return (
+    <div className={wraperClass} style={{ backgroundImage: `url(${blobSrc})` }}>
+      <img className="size-32 my-4" src={iconSrc} alt="icon" />
+      <h1 className={titleClass}>{title}</h1>
+    </div>
   );
 }
