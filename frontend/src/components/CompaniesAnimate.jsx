dcf74ffb969b42a8a2e0b615e07921d6d08f15d6
Dev-Sahani <devdaksh8102003@gmail.com>
2024-10-26 03:15:29 +0530

    Companies animate refractored

--- frontend/src/components/CompaniesAnimate.jsx
+++ frontend/src/components/CompaniesAnimate.jsx
@@ -1,104 +1,32 @@
-import { motion } from "framer-motion";
 import classNames from "classnames";
-import { useWinSizeContext } from "../context/winSizeContext";
-import { useEffect, useState } from "react";
 
-export default function CompaniesAnimate({children, className, direction=1, length, ...other}) {
-  const classes = classNames("absolute left-0", {
+export default function CompaniesAnimate({
+  children,
+  className,
+  widthComapreToScreen = 1,
+  ...other
+}) {
+  if (widthComapreToScreen < 1) widthComapreToScreen = 1;
+
+  const classes = classNames("absolute left-0 relative overflow-hidden group", {
     [className]: className,
   });
-  const classes2 = classNames("absolute left-0 bottom-0", {
-    [className]: className,
-  });
-
-  const {
-    winSize,
-    BR_1, 
-    BR_3, 
-    BR_4, 
-  } = useWinSizeContext()
-  
-  // const [state, setState] = useState({gap:"", start:"", middle:0, end:""});
-  // useEffect(()=>{
-  //   const gap = winSize <= BR_1 ? "4rem" : (winSize <= BR3 ? "7rem" : "8rem");
-  //   setState({
-  //     gap,
-  //     start: `calc(0px - 100% - ${gap})`,
-  //     middle: 0,
-  //     end: `calc(100% - 0px + ${gap})`,
-  //   })
-  // }, [winSize]);
-  const gap = winSize <= BR_1 ? "4rem" : (winSize <= BR_3 ? "7rem" : (winSize <= 1440 ? "8.5rem" : "9.9rem"));
-  console.log(gap);
-  const start = `calc(0px - 100% - ${gap})`;
-  const middle = 0;
-  const end = `calc(100% - 0px + ${gap})`;
 
   return (
-    <>
-      <motion.ul 
-            className={classes}
-            initial={{x: direction > 0 ? start : end}}
-            animate={{
-              x: middle,
-              transition: {
-                duration: 30,
-                ease: "linear",
-                repeat: Infinity,
-              }
-            }}
-      >
+    <div
+      className={classes}
+      style={{ width: 100 * widthComapreToScreen + "vw" }}
+      {...other}
+    >
+      <ul className="absolute w-full flex justify-between items-center gap-8 animate-scroll-auto group-hover:pause-animation">
+        <div className="h-full" />
         {children}
-      </motion.ul>
-      <motion.ul 
-            className={classes}
-            initial={{x : middle}}
-            animate={{
-              x: direction > 0 ? end : start,
-              transition: {
-                duration: 30,
-                ease: "linear",
-                repeat: Infinity, 
-              }
-            }}
-      >   
-        {children}
-      </motion.ul>
-      {/* {
-        winSize <= BR_1 && 
-        <>
-        <motion.ul 
-            className={classes2}
-            initial={{x: direction < 0 ? start : end}}
-            animate={{
-              x: middle,
-              transition: {
-                duration: 30,
-                ease: "linear",
-                repeat: Infinity,
-              }
-            }}
-      >
-        {children}
-      </motion.ul>
-      <motion.ul 
-            className={classes2}
-            initial={{x : middle}}
-            animate={{
-              x: direction < 0 ? end : start,
-              transition: {
-                duration: 30,
-                ease: "linear",
-                repeat: Infinity, 
-              }
-            }}
-      >   
-        {children}
-      </motion.ul>
-      </>
-      } */}
-    </>   
-  )
-}
+      </ul>
 
-// flex justify-start items-center gap-20
+      <ul className="absolute w-full flex justify-between items-center gap-8 animate-scroll-auto-delayed group-hover:pause-animation">
+        <div className="h-full" />
+        {children}
+      </ul>
+    </div>
+  );
+}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,89 +1,93 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-    }
-    ,extend: {
-
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    extend: {
       boxShadow: {
-        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
-        'xl':'0 0 10px rgba(0,0,0,0.2)'
+        "3xl": "0 35px 60px -15px rgba(0, 0, 0, 0.3)",
+        xl: "0 0 10px rgba(0,0,0,0.2)",
       },
       fontFamily: {
-        'jost': ['Jost', 'sans-serif'],
-        "playfair-display": ["Playfair-Display", "serif"]
+        jost: ["Jost", "sans-serif"],
+        "playfair-display": ["Playfair-Display", "serif"],
       },
       colors: {
         // ...colors,
-        primary : {
-          100: '#FFE9DD',
-          200: '#FFC9AA',
+        primary: {
+          100: "#FFE9DD",
+          200: "#FFC9AA",
           300: "#FFB78E",
-          400: '#FFA572',
-          500: '#FF9356',
-          600: '#D47B48',
-          700: '#AA6239 ',
-          800: '#804A2B',
-          900: '#55311D',
-          1000: "#331D11"
+          400: "#FFA572",
+          500: "#FF9356",
+          600: "#D47B48",
+          700: "#AA6239 ",
+          800: "#804A2B",
+          900: "#55311D",
+          1000: "#331D11",
         },
         secondary: {
-          100: '#CECDCD',
-          200: '#838181',
+          100: "#CECDCD",
+          200: "#838181",
           300: "#5A5857",
-          400: '#312E2D',
-          500: '#080403',
-          600: '#070303',
-          700: '#050302',
-          800: '#040202',
-          900: '#030101',
+          400: "#312E2D",
+          500: "#080403",
+          600: "#070303",
+          700: "#050302",
+          800: "#040202",
+          900: "#030101",
           1000: "#020101",
         },
         tertiary: {
           500: "#F4F4F4",
           600: "#E4E4E4",
           800: "#BBBBBB",
-
         },
-        additional:{
-          200:"#F7F7F7"
-        }
+        additional: {
+          200: "#F7F7F7",
+        },
       },
-      keyframes:{
-        "scroll-up-icon":{
+      keyframes: {
+        "scroll-up-icon": {
           "0%, 100%": {
             transform: "translateY(0rem)",
-            opacity: 1
+            opacity: 1,
           },
-          "35%":{
+          "35%": {
             transform: "translateY(-0.5rem)",
-            opacity: 0
+            opacity: 0,
           },
           "70%": {
             transform: "translateY(0.5rem)",
-            opacity: 0
+            opacity: 0,
           },
+        },
+        "scroll-auto": {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(100%)" },
+        },
+        "scroll-delay-auto": {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(0)" },
         },
       },
       animation: {
-        "scroll-up": 'scroll-up-icon 1s ease-in-out infinite',
+        "scroll-up": "scroll-up-icon 1s ease-in-out infinite",
+        "scroll-auto": "scroll-auto 20s linear infinite",
+        "scroll-auto-delayed": "scroll-delay-auto 20s linear infinite",
       },
       padding: {
         "screen-padding": "6vw",
       },
-      backgroundSize:{
-        "55%":"55%"
-      }
+      backgroundSize: {
+        "55%": "55%",
+      },
     },
   },
   plugins: [],
-}
-
+};
